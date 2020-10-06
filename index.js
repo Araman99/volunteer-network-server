f75e8af0e8d43924d627c3dd7362170a8681545c
Amanur Rahman <aramancse@gmail.com>
2020-10-07 01:52:26 +0600

    error fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
+require('dotenv').config()
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u2cqf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -27,8 +28,8 @@ app.get('/user/:id',(req,res) =>{
 
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true  });
 client.connect(err => {
-  const collection = client.db(`${process.env.DB_PASS}`).collection("volunteerTask");
-  const registered = client.db(`${process.env.DB_PASS}`).collection("registerVolunteer")
+  const collection = client.db(`volunteerNetwork`).collection("volunteerTask");
+  const registered = client.db(`volunteerNetwork`).collection("registerVolunteer")
   
   app.get('/task', (req,res)=>{
     collection.find({}).limit(20)
@@ -78,6 +79,6 @@ client.connect(err => {
 });
 
 const port = 5000;
-app.listen(process.env.PORT ||port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
