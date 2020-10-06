4cb63ffc7ce804f8c4894e6d245c4aa101085402
Amanur Rahman <aramancse@gmail.com>
2020-10-06 23:04:28 +0600

    done

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
 const express = require('express')
-const app = express()
-const port = 5000
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
+
+
+const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.u2cqf.mongodb.net/DB_NAME?retryWrites=true&w=majority";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/user/:id',(req,res) =>{
+  res.send('hey there')
+  console.log(req.params);
+})
 
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true  });
+client.connect(err => {
+  const collection = client.db("volunteerNetwork").collection("volunteerTask");
+  const registered = client.db("volunteerNetwork").collection("registerVolunteer")
+  
+  app.get('/task', (req,res)=>{
+    collection.find({}).limit(20)
+    .toArray((error, documents)=>{
+      res.send(documents)
+    })
+  })
+
+  app.post('/submitForm', (req,res) => {
+    registered.insertOne(req.body)
+    .then(result => {
+      console.log(res.send(result.insertedCount>0));
+    })
+  })
+
+  app.get('/addedEvent',(req,res)=>{
+    registered.find({email:req.headers.email})
+    .toArray((error,documents)=>{
+      res.send(documents)
+      
+    })
+  })
+
+  app.get('/allRegisteredEvent',(req,res)=>{
+    registered.find({})
+    .toArray((error,documents)=>{
+      res.send(documents)
+      
+    })
+  })
+
+  app.delete('/cancelEvent',(req,res)=>{
+    console.log(req.headers.id);
+    registered.deleteOne({_id: ObjectID(req.headers.id)})
+    .then(result=>{
+      res.send(result.deletedCount>0)
+    })
+  })
+
+  app.delete('/deleteEvent',(req,res)=>{
+    registered.deleteOne({_id:ObjectID(req.headers.id)})
+    .then(result=>{
+      res.send(result.deletedCount>0)
+    })
+  })
+  
+});
+
+const port = 5000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
